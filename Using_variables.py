730d2d0744e6abeff3b9df14da604b3e5192f6d4
Abaruwa4 <tola-baruwa@hotmail.co.uk>
2025-03-24 17:22:10 +0000

    Using variables in Python

---
+++ Using_variables.py
@@ -0,0 +1,15 @@
+# Create and Call a variable
+device_id = "h32rb17"
+print (device_id)
+
+# Return the data type of a variable
+device_id = "h32rb17"
+data_type = (device_id)
+print (data_type)
+
+# Reassign a variable
+device_id = "h32rb17"
+print (device_id)
+device_id = "n73ab07"
+print (device_id)
+# The original device ID followed by the new Device ID will be returned
