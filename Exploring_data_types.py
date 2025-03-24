609d116d9ee43924709313bc40fe139af4a8e2c7
Abaruwa4 <tola-baruwa@hotmail.co.uk>
2025-03-24 17:05:15 +0000

    Exploring different Python data types

---
+++ Exploring_data_types.py
@@ -0,0 +1,27 @@
+# String data - Print Hello Python
+print ("Hello Python!")
+
+# Float data - Add 2.5 and 1.5
+print (2.5 + 1.5)
+
+# Integer data - Divide 20 by 5
+print ( 20/5)
+# To return a whole number - number is rounded down to the nearest whole number
+print (1//4)
+# 0 rather than 0.25 will be returned
+
+# Boolean data - Less than or Greater than - True/ False
+print (10 < 5)
+print (9 < 12)
+
+# List data - Print a list
+print (["Sonia", "Ahmed", "Daniel", "Abraham"])
+
+# Dictionary data - data with one or more key-value pairs
+# Match a number to a building structure
+a = {1 : "East",
+ 2 : "West",
+ 3 : "North",
+ 4 : "South"}
+# The number is the key and the building name is the value
+print(a)
