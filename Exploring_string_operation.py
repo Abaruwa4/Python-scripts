e6ec12854779a05d20ff2affe3a7dbfb28ab003b
Abaruwa4 <tola-baruwa@hotmail.co.uk>
2025-03-25 20:36:11 +0000

    Exploring string operations in Python

---
+++ Exploring_string_operation.py
@@ -0,0 +1,28 @@
+# Convert an integer into a string
+new_string = str (123)
+print (type(new_string))
+
+# Use len
+# Print the length of a string - "hello"
+print (len("Hello"))
+
+# String concatenation - join two strings together
+print ("Hello" + "World")
+
+# Use upper
+print ("hello". upper())
+
+# Use lower
+print ("HELLO".lower())
+
+# Explore string indices
+print ("Hello" [1])
+
+# Explore splicing
+print ("Hello" [1:4])
+
+# Use the string index method
+fruits = ["apples", "mangoes", "bananas", "pineapples", "strawberries"]
+x = fruits.index("mangoes")
+print (x)
+
